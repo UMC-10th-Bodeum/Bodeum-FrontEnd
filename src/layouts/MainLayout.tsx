4318c844cbe3c6ce3aed9ce05451cdeb37fad8da
rladmsgki <rladmsgki@naver.com>
2026-07-06 20:58:25 +0900

    feat: Topbar 구현

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import SideBar from "./components/sidebar/SideBar";
+import TopBar from "./components/TopBar";
 
 export default function MainLayout() {
   return (
@@ -7,7 +8,7 @@ export default function MainLayout() {
       <SideBar />
 
       <div className="flex flex-1 flex-col">
-        {/* <TopBar /> */}
+        <TopBar />
 
         <main className="flex-1 overflow-y-auto">
           <Outlet />
---
+++ src/layouts/components/TopBar.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import Input from "../../components/Input";
+
+export default function TopBar() {
+  const [keyword, setKeyword] = useState("");
+
+  return (
+    <header className="h-[60px] border-b border-background-250 bg-white px-[20px] py-[8px]">
+      <Input
+        search
+        value={keyword}
+        onChange={(e) => setKeyword(e.target.value)}
+        placeholder="기관 · 병원 · 복지 · 취업 · 교육 정보를 검색해보세요"
+      />
+    </header>
+  );
+}
