66cfc5c0c023f8ede97c766ac3511c5d0677cb61
Jungbin906 <jungbin906@naver.com>
2026-07-07 15:06:02 +0900

    fix : 테스트코드 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,10 @@
 import './App.css'
-import MainButton from './components/MainButton'
 
 function App() {
   return (
-    <main className="min-h-screen bg-background-100 p-10">
-      <div className="flex flex-col items-start gap-4">
-        <MainButton size="L">다음</MainButton>
-        <MainButton size="L" disabled>
-          다음
-        </MainButton>
-
-        <MainButton size="M">다음</MainButton>
-        <MainButton size="M" disabled>
-          다음
-        </MainButton>
-
-        <MainButton size="S">다음</MainButton>
-        <MainButton size="S" disabled>
-          다음
-        </MainButton>
-
-        <MainButton size="S" stroke>
-          다음
-        </MainButton>
-        <MainButton size="S" stroke disabled>
-          다음
-        </MainButton>
-      </div>
-    </main>
+    <>
+      <h1 className="text-h5-list text-sub-green">보듬</h1>
+    </>
   )
 }
 
