42ecadf799b0e060f56bbaa73af2883b83efe724
Jungbin906 <jungbin906@naver.com>
2026-07-08 20:24:28 +0900

    test : 테스트 페이지 삭제

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,18 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import EmptyLayout from "../layouts/EmptyLayout";
 import MainLayout from "../layouts/MainLayout";
-import MainButtonTestPage from "../pages/MainButtonTestPage";
 import HomePage from "../pages/home/HomePage";
 
 export const router = createBrowserRouter([
   {
     element: <EmptyLayout />,
-    children: [
-      {
-        path: "/main-button-test",
-        element: <MainButtonTestPage />,
-      },
-    ],
+    // children: [
+    //   {
+    //     path: "/onboarding",
+    //     element: <OnboardingPage />,
+    //   },
+    // ],
   },
   {
     element: <MainLayout />,
