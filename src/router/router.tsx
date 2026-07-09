46f11bc2448250f23404ada70937751037b7f45d
Jungbin906 <jungbin906@naver.com>
2026-07-09 13:43:08 +0900

    test : 테스트 페이지 제거

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,24 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
-import EmptyLayout from "../layouts/EmptyLayout";
 import MainLayout from "../layouts/MainLayout";
-import OnboardCancelBoxTestPage from "../pages/OnboardCancelBoxTestPage";
-import OnboardBoxFrameTestPage from "../pages/OnboardBoxFrameTestPage";
 import HomePage from "../pages/home/HomePage";
 
 export const router = createBrowserRouter([
-  {
-    element: <EmptyLayout />,
-    children: [
-      {
-        path: "/onboard-cancel-box-test",
-        element: <OnboardCancelBoxTestPage />,
-      },
-      {
-        path: "/onboard-box-frame-test",
-        element: <OnboardBoxFrameTestPage />,
-      },
-    ],
-  },
   {
     element: <MainLayout />,
     children: [
