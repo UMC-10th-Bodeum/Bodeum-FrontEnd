c19cdfe606e1f59f2bf7c20fc466978539011462
rladmsgki <rladmsgki@naver.com>
2026-07-10 21:05:23 +0900

    fix: button -> div 태그로 수정

--- src/components/RegionLabel.tsx
+++ src/components/RegionLabel.tsx
@@ -1,20 +1,17 @@
 interface RegionLabelProps {
   region: string;
-  onClick?: () => void;
 }
 
 export default function RegionLabel({
-  region,
-  onClick,
+  region
 }: RegionLabelProps) {
   return (
-    <button
-      onClick={onClick}
+    <div
       className="inline-flex h-[20px] items-center justify-center rounded-[6px] bg-main-150 px-[8px]"
     >
       <span className="text-body-label text-main-400">
         {region}
       </span>
-    </button>
+    </div>
   );
 }
