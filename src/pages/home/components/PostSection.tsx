11e948b8354ee79f876ff4cad3b5a739805db264
rladmsgki <rladmsgki@naver.com>
2026-07-13 22:41:52 +0900

    feat: 추천 소식 Section UI 구현

---
+++ src/pages/home/components/PostSection.tsx
@@ -0,0 +1,30 @@
+import RightIcon from "@/assets/icons/ChevronRight.svg?react";
+
+interface PostSectionProps {
+  title: string;
+  children: React.ReactNode;
+  onMoreClick?: () => void;
+}
+
+export default function PostSection({
+  title,
+  children,
+  onMoreClick,
+}: PostSectionProps) {
+  return (
+    <section className="w-full">
+      <div className="mb-[16px] flex items-center justify-between border-b border-background-300 pb-[8px]">
+        <h2 className="text-h6 text-background-600">{title}</h2>
+
+        <button
+          onClick={onMoreClick}
+          className="text-h6-list text-background-500 flex items-center gap-[4.66px] cursor-pointer">
+          더보기
+          <RightIcon className="text-background-500" />
+        </button>
+      </div>
+
+      <div className="flex flex-col gap-[8px]">{children}</div>
+    </section>
+  );
+}
