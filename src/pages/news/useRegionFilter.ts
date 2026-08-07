581f61912833a66d0c8b5222a6b633642216c3e0
Jungbin906 <jungbin906@naver.com>
2026-08-07 19:40:11 +0900

    fix: 뉴스 지역 필터 유틸 import 경로 수정

--- src/pages/news/useRegionFilter.ts
+++ src/pages/news/useRegionFilter.ts
@@ -2,7 +2,8 @@ import { useMyProfile } from "@/hooks/useMyPage";
 import { useEffect, useState } from "react";
 import { ALL_REGIONS_LABEL, ALL_REGIONS_VALUE } from "./components/RegionOnboardingBox";
 import { formatRegionDisplayLabel } from "@/constants/regions";
-import { findRegionId, getRegions } from "@/apis/onboardingApi";
+import { getRegions } from "@/apis/onboardingApi";
+import { findRegionId } from "@/utils/onboarding";
 
 type RegionCommit = {
   regionLevel1: string | null;
