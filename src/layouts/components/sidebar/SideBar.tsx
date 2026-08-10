5b08c04ea3ec504c1c6ba0f5c403160db15f7152
rladmsgki <rladmsgki@naver.com>
2026-08-10 17:31:42 +0900

    feat: 로고 버튼 활성화

--- src/layouts/components/sidebar/SideBar.tsx
+++ src/layouts/components/sidebar/SideBar.tsx
@@ -95,19 +95,23 @@ export default function SideBar() {
 
   return (
     <aside className="flex h-screen flex-col bg-white w-[213px]">
-      <button
-        className="h-[60px] py-[18px] flex items-center justify-center border-b border-background-250 cursor-pointer"
-        onClick={() => {
-          if (location.pathname === "/") {
-            window.location.reload();
-            return;
-          }
-
-          navigate("/");
-        }}
+      <div
+        className="h-[60px] py-[18px] flex items-center justify-center border-b border-background-250"
       >
-        <Logo className="w-[62px] h-[24px]" />
-      </button>
+        <button
+          type="button"
+          className="cursor-pointer"
+          onClick={() => {
+            if (location.pathname === "/") {
+              window.location.reload();
+              return;
+            }
+            navigate("/");
+          }}
+        >
+          <Logo className="h-[24px] w-[62px]" />
+        </button>
+      </div>
 
       <div className="flex flex-1 flex-col border-r border-background-250">
         <div className="px-4 pt-[20px]">
