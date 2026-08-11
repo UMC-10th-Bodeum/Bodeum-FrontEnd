aecf70896f10449a6336bda1270c3a7eacd1cf18
rladmsgki <rladmsgki@naver.com>
2026-08-11 21:15:05 +0900

    feat: 정보- 청소년 수련관, KEAD 취업정보 칩 삭제

--- src/pages/info/components/CategoryChips.tsx
+++ src/pages/info/components/CategoryChips.tsx
@@ -19,9 +19,15 @@ export default function CategoryChips({
 }: CategoryChipsProps) {
   const categories = infoSubCategoryMap[parentCategory];
 
+  const visibleCategories = categories.filter(
+    (item) =>
+      item.value !== "YOUTH_CENTER" &&
+      item.value !== "KEAD_JOB",
+  );
+
   return (
     <div className="flex flex-wrap gap-2">
-      {categories
+      {visibleCategories
         .filter((item) => item.value.endsWith("_ETC"))
         .map((item) => (
           <CategoryButton
@@ -41,7 +47,7 @@ export default function CategoryChips({
         onClick={() => onChange(null)}
       />
 
-      {categories
+      {visibleCategories
         .filter((item) => !item.value.endsWith("_ETC"))
         .map((item) => (
           <CategoryButton
